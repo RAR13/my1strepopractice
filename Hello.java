a35fb04183e4bd66c69a4b3634d11bc4774930b4
Rose Angel <roseangelroy1@gmail.com>
2021-10-28 10:35:07 +0000

    resolved conflicts

--- Hello.java
+++ Hello.java
@@ -2,7 +2,6 @@ class Hello
 {
 public static void main(String args[])
 {
-System.out.println("hello world, you are awesome & wonderful. I am feature and I am now feature102");
-}
+System.out.println("hello world, you are awesome & wonderful. I am feature and I am now feature102" Modifying to resolve conflict}
 }
 
